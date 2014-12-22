73d2c738fcd3e033060fb7d4836500f67579f316
Friedrich Delgado Friedrichs <friedel@nomaden.org>
2014-12-22 20:57:58 +0100

    add requirements.txt
    (rudimentary, I guess)

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(name='dailystrips',
       version='0.0',
       description='Fetch my daily cartoons from the web',
       author='Friedrich Delgado',
       author_email='friedel@nomaden.org',
       url='https://github.com/TauPan/dailystrips.py',
-      packages=['dailystrips']
-)
+      packages=['dailystrips'],
+      install_requires=required)
