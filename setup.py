5138572a6ac411a78acdd2cb7481ca1c2eb6d70b
Friedrich Delgado Friedrichs <friedel@nomaden.org>
2014-12-22 20:52:47 +0100

    skeleton setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dailystrips',
+      version='0.0',
+      description='Fetch my daily cartoons from the web',
+      author='Friedrich Delgado',
+      author_email='friedel@nomaden.org',
+      url='https://github.com/TauPan/dailystrips.py',
+      packages=['dailystrips']
+)
