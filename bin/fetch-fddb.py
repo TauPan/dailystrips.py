dd17c871a20cfcc0123227d10f080f559f9d851c
Friedrich Delgado Friedrichs <friedel@nomaden.org>
2017-12-24 09:46:25 +0100

    factor out scrape_foods

--- bin/fetch-fddb.py
+++ bin/fetch-fddb.py
@@ -35,28 +35,7 @@ def main(argv):
             page = lxml.html.document_fromstring(content)
             calorific = ''.join(page.xpath('//td[span/b="Brennwert"]/following-sibling::td//text()'))
             kcal_total = int(re.search('(\d+) kcal', calorific).group(1))
-            day_rows = page.xpath('//table[@class="myday-table-std"]//tr')
-            foods = []
-            for row in day_rows:
-                time_el = row.xpath('td[1]/span[@class="mydayshowtime"]/text()')
-                if not time_el:
-                    continue
-                time = dateutil.parser.parse(time_el[0], default=date)
-                food = row.xpath('td[1]/a')[0]
-                food_link = food.attrib['href']
-                food_txt = food.text
-                kcal = int(re.search('(\d+) kcal', row.xpath('td[3]//text()')[0]).group(1))
-                fat = float((re.search('([\d,]+) g', row.xpath('td[4]//text()')[0]).group(1)).translate(str.maketrans(',', '.')))
-                carbs = float((re.search('([\d,]+) g', row.xpath('td[5]//text()')[0]).group(1)).translate(str.maketrans(',', '.')))
-                protein = float((re.search('([\d,]+) g', row.xpath('td[6]//text()')[0]).group(1)).translate(str.maketrans(',', '.')))
-                foods.append(dict(
-                    time=time,
-                    food=dict(link=food_link,
-                              text=food_txt),
-                    kcal=kcal,
-                    fat=fat,
-                    carbs=carbs,
-                    protein=protein))
+            foods = scrape_foods(page, date)
             day = dict(
                 date=date,
                 kcal=kcal_total,
@@ -65,6 +44,32 @@ def main(argv):
             date = nextdate
         import pdb; pdb.set_trace()
         pass
+
+
+def scrape_foods(page, date):
+    food_rows = page.xpath('//table[@class="myday-table-std"]//tr')
+    foods = []
+    for row in food_rows:
+        time_el = row.xpath('td[1]/span[@class="mydayshowtime"]/text()')
+        if not time_el:
+            continue
+        time = dateutil.parser.parse(time_el[0], default=date)
+        food = row.xpath('td[1]/a')[0]
+        food_link = food.attrib['href']
+        food_txt = food.text
+        kcal = int(re.search('(\d+) kcal', row.xpath('td[3]//text()')[0]).group(1))
+        fat = float((re.search('([\d,]+) g', row.xpath('td[4]//text()')[0]).group(1)).translate(str.maketrans(',', '.')))
+        carbs = float((re.search('([\d,]+) g', row.xpath('td[5]//text()')[0]).group(1)).translate(str.maketrans(',', '.')))
+        protein = float((re.search('([\d,]+) g', row.xpath('td[6]//text()')[0]).group(1)).translate(str.maketrans(',', '.')))
+        foods.append(dict(
+            time=time,
+            food=dict(link=food_link,
+                      text=food_txt),
+            kcal=kcal,
+            fat=fat,
+            carbs=carbs,
+            protein=protein))
+    return foods
 
 
 @contextmanager
